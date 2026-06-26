f532263268267798f4e9fa91b430a9ae0451d0fc
Muhofy <muhofy@iris.app>
2026-06-26 14:42:51 +0000

    fix(shell): remove LD_PRELOAD exec hook, unused with targetSdk=28

--- app/src/main/java/com/iris/assistant/data/shell/IrisShellSession.kt
+++ app/src/main/java/com/iris/assistant/data/shell/IrisShellSession.kt
@@ -64,9 +64,6 @@ class IrisShellSession @Inject constructor(
         }
         if (!shellBin.canExecute()) shellBin.setExecutable(true)
 
-        val nativeLibDir = context.applicationInfo.nativeLibraryDir
-        val execHook = "$nativeLibDir/libiris-exec-hook.so"
-
         val envVars = arrayOf(
             "HOME=$homePath",
             "PREFIX=$prefixPath",
@@ -75,7 +72,6 @@ class IrisShellSession @Inject constructor(
             "TERM=xterm-256color",
             "LANG=en_US.UTF-8",
             "LD_LIBRARY_PATH=$prefixPath/lib",
-            "LD_PRELOAD=$execHook",
             "TERMUX_PREFIX=$prefixPath",
         )
 
